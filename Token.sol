37c7a0acc9a1a9003c9a62a6849e9449294ef3be
chdl1314 <88882964+chdl1314@users.noreply.github.com>
2021-09-28 12:49:53 +0800

    DDM

--- Token.sol
+++ Token.sol
@@ -16,7 +16,7 @@ contract Token is TRC20, TRC20Detailed {
     /**
      * @dev Constructor that gives msg.sender all of existing tokens.
      */
-    constructor () public TRC20Detailed("ZXYD", "ZXYD", 6) {
-        _mint(msg.sender, 88888 * (10 ** uint256(decimals())));
+    constructor () public TRC20Detailed("DDM", "DDM", 6) {
+        _mint(msg.sender, 8888 * (10 ** uint256(decimals())));
     }
 }
